e4f9bda26f66b3182237c8075d83526f53d703e2
derekarani <derekarani@gmail.com>
2017-05-04 08:16:32 +0300

    Code is able to check out all three criterias of determimning a laep year efficiently

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,23 @@ $(document).ready(function() {
 
 
 
-    alert("After the leapyear")
+
 
     var year = parseInt($("input#year").val());
 
+
     var leapYear = function(year) {
-      alert("Just before if")
-      if (year % 4 === 0)
-       return true;}
-    //  } else {
-    //    return false;
-    //  };
-     var result = leapYear(year);
+
+      if ((year % 4 === 0) && (year %100 !== 0) && (year % 400 === 0)) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+    var result = leapYear(year);
 
     $("#result").text(result);
+
 
   });
 });
